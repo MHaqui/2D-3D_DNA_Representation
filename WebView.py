69a7e8fc060ef846cd7ae965a1ba98d350cf2914
Maxime Haquin <maxime.haquinlage@gmail.com>
2021-07-21 09:06:02 +0200

    test WebView

---
+++ WebView.py
@@ -0,0 +1,22 @@
+from posixpath import abspath
+import sys
+import PySide2
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWebEngineWidgets import *
+import os
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    view = QWebEngineView()
+    view.settings().setAttribute(
+        QWebEngineSettings().LocalContentCanAccessRemoteUrls, True
+    )
+    view.load(
+        QUrl().fromLocalFile(
+            os.path.split(os.path.abspath(__file__))[0] + r"/test.html"
+        )
+    )
+    view.show()
+    app.exec_()
