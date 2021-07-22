7373d6a4c9b7ebbb1b51651688168ae9a9b102a4
Maxime Haquin <maxime.haquinlage@gmail.com>
2021-07-22 18:05:59 +0200

    Add fonctionalities

--- WebView.py
+++ WebView.py
@@ -7,12 +7,17 @@ from PySide2.QtCore import *
 from PySide2.QtWebEngineWidgets import *
 import os
 
+# from ..Domain_checking import js
+# from . import ngl as NGL
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     view = QWebEngineView()
     view.settings().setAttribute(
         QWebEngineSettings().LocalContentCanAccessRemoteUrls, True
     )
+    view.settings().setAttribute(QWebEngineSettings().FullScreenSupportEnabled, True)
     view.load(
         QUrl().fromLocalFile(
             os.path.split(os.path.abspath(__file__))[0] + r"/test.html"
